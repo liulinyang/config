6c96f7a0ea6aadc1ee462683a208ff74d1e46b90
liulinyang <liulinyang@gmail.com>
2013-07-16 14:09:31 +0800

    add another shell command to download ftp folder with wget --recursive

--- common.sh
+++ common.sh
@@ -1,1 +1,6 @@
+# Upload file to ftp server via curl 
 curl -T Screenshots.zip -u test:testtest ftp://10.204.16.2/home/ics/kevin/
+
+
+# Using wget to download the directory from ftp server and only keep the last part of URL as the local folder name
+wget --no-verbose --no-parent --recursive --user=test --password=testtest --no-host-directories --cut-dirs=6 ftp://10.204.16.2/Build/ICSS/1.2/centos6_64/en/Rel/1143/
